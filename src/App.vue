e047ab4a3160bf31263aeee3715755b62689bccd
Sébastien Merveille <sebastien.merveille@ac.andenne.be>
2023-11-09 18:01:59 +0100

    Router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,6 @@
 <script setup>
-import { ref } from 'vue'
-import card from './components/card.vue'
-
-const pokemonList = ref([])
-
-fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-  .then(response => response.json())
-  .then(data => {
-    pokemonList.value = data.results.map(pokemon => pokemon.name)
-  })
-  .catch(error => console.log(error))
-
 </script>
 
 <template>
-  <div class="grid_poke" id="grid_poke">
-    <card v-for="pokemon in pokemonList" :key="pokemon" :name="pokemon"></card>
-  </div>
+  <router-view></router-view>
 </template>
-
-<style>
-.grid_poke {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  grid-gap: 1rem;
-}
-</style>
